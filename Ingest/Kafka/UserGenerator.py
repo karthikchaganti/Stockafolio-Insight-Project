93883bb67298765805f6bf93515e53f2fb27d1e9
Karthik <karthikchaganti25@gmail.com>
2017-02-04 21:18:40 -0500

    pickle working

--- Ingest/Kafka/UserGenerator.py
+++ Ingest/Kafka/UserGenerator.py
@@ -2,24 +2,25 @@ import random
 import pickle
 import uuid
 from faker import Faker
-
+import sys
 
 class UserGen():
 
     def __init__(self,userCount):
         self.userCount = userCount
-        fake = Faker()
+        self.faker = Faker()
         self.userList_dict = {}
 
     def userList(self):
+        output = open('userList.pkl', 'wb')
         for i in range(int(self.userCount)):
             user_ID = str(uuid.uuid4()) # random
             user_Name =self.faker.name()
             self.userList_dict[user_ID] = user_Name
-            pickle.dump(userList_dict, output)
-            output.close()
+            pickle.dump(self.userList_dict, output)
+        output.close()
 
-if __name__ == "main":
+if __name__ == "__main__":
     args = sys.argv
     userCount = str(args[1])
     obj_UserGen = UserGen(userCount)
