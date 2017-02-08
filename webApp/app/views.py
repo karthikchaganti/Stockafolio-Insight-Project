4a309af5a01a8fb5a26eaf65f6371d6026980377
Karthik <karthikchaganti25@gmail.com>
2017-02-08 17:24:25 -0500

    updated some html

--- webApp/app/views.py
+++ webApp/app/views.py
@@ -13,19 +13,19 @@ cluster = Cluster(["ec2-34-198-185-77.compute-1.amazonaws.com"])
 session = cluster.connect("stockportfolio")
 session.default_fetch_size = None
 
-def get_user_data(userid):
+def get_user_data(user):
     # Read user's data from cassandra dB
-    hist_trades = session.execute("SELECT tickername, tradequantity,tickerprice,total_val,tradetype,tradetime FROM db_trades_stream  WHERE userId=%s ORDER BY tradetime DESC LIMIT 5", parameters=[userid])
-    port_list   = session.execute("SELECT tickername, tickerquant, tickervalue FROM db_user_portfolio WHERE userid=%s", parameters=[userid])
-    port_total  = session.execute("SELECT portfolio_count, portfolio_value FROM db_user_portcount WHERE userid=%s", parameters=[userid])
+    hist_trades = session.execute("SELECT tickername, tradequantity,tickerprice,total_val,tradetype,tradetime FROM db_trades_stream  WHERE userid=%s ORDER BY tradetime DESC LIMIT 5", parameters=[user])
+    port_list   = session.execute("SELECT tickername, tickerquant, tickervalue FROM db_user_portfolio WHERE userid=%s", parameters=[user])
+    port_total  = session.execute("SELECT portfolio_count, portfolio_value FROM db_user_portcount WHERE userid=%s", parameters=[user])
     return [hist_trades,port_list,port_total]
 
-def get_user_json_data(userid):
+def get_user_json_data(user):
 
     hist_trades, port_list, port_total = get_user_data
-    hist_trades_json = [{"tickername":row.tickername, "tradequantity":row.tradequantity,"tickerprice":row.tickerprice,"total_val":row.total_val,"tradetype":row.tradetype,"tradetime":row.tradetime}for row in hist_trades]
-    port_list_json   = [{"tickername":row.tickername,"tickerquant":tickerquant,"tickervalue":tickervalue} for row in port_list]
-    port_total_json  = [{"portfolio_count":portfolio_count,"portfolio_value":portfolio_value} from row in port_total]
+    hist_trades_json = [{"tickername":row.tickername, "tradequantity":row.tradequantity,"tickerprice":row.tickerprice,"total_val":row.total_val,"tradetype":row.tradetype,"tradetime":row.tradetime}]for row in hist_trades
+    port_list_json   = [{"tickername":row.tickername,"tickerquant":tickerquant,"tickervalue":tickervalue}] for row in port_list
+    port_total_json  = [{"portfolio_count":row.portfolio_count,"portfolio_value":row.portfolio_value}] from row in port_total
 
     return jsonify(hist_trades = hist_trades_json,port_list=port_list_json,port_total=port_total_json)
 
@@ -38,8 +38,12 @@ def index():
 def slides():
     return render_template("slides.html")
 
-@app.route('/userid')
+@app.route('/user')
 def get_user():
-    user=request.args.get("userid")
-    hist_trades,port_list,port_total = get_user_data(request.args.get("userid"))
-    return render_template("userid.html", userid=userid, hist_trades = hist_trades, port_list = port_list, port_total = port_total)
+    user=request.args.get("user")
+    hist_trades,port_list,port_total = get_user_data(request.args.get("user"))
+    return render_template("userhi.html", user=user, hist_trades = hist_trades, port_list = port_list, port_total = port_total)
+
+@app.route('/retrieve_user_data')
+def retrieve_user_data():
+    return get_user_json_data(request.args.get("user"))
--- webApp/tornadoapp.py
+++ webApp/tornadoapp.py
@@ -1,5 +1,4 @@
-#! /home/ubuntu/miniconda3/bin/python3.5
-
+#! /usr/bin/env python
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
 from tornado.web import FallbackHandler, RequestHandler, Application
@@ -12,9 +11,7 @@ class MainHandler(RequestHandler):
 tr = WSGIContainer(app)
 
 application = Application([
-(r"/tornado", MainHandler),
-(r".*", FallbackHandler, dict(fallback=tr)),
-])
+(r"/tornado", MainHandler),(r".*", FallbackHandler, dict(fallback=tr)),])
 
 if __name__ == "__main__":
  application.listen(80)
