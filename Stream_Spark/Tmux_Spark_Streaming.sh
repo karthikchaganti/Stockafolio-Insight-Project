55611ed6cfd36c6221a8099047f593b8146cedf7
Ubuntu <ubuntu@ip-172-31-0-100.ec2.internal>
2017-02-10 06:12:40 +0000

    Tmux Spark Working

--- Stream_Spark/Tmux_Spark_Streaming.sh
+++ Stream_Spark/Tmux_Spark_Streaming.sh
@@ -1,18 +1,9 @@
-SESSION='de-ny-karthik-sparkStreaming'
-
-# New tmux session
+#!/bin/bash
+SESSION=de-ny-karthik-spark
+ID=1
 tmux new-session -s $SESSION -n bash -d
-
-# Create a new window for electricity data
-ElecID=1
-tmux new-window -t $ElecID
-tmux send-keys -t $SESSION:$ElecID 'PYSPARK_PYTHON=/home/ubuntu/miniconda3/bin/python3.5 spark-submit --master spark://ip-172-31-0-100:7077 --jars spark-streaming-kafka-0-8-assembly_2.11-2.1.0.jar Trades.py' C-m
-
-
-
-
-
-
+tmux new-window -t $ID
+tmux send-keys -t $SESSION:$ID 'PYSPARK_PYTHON=/home/ubuntu/miniconda3/bin/python3.5 spark-submit --master spark://ip-172-31-0-100:7077 --jars /home/ubuntu/Insight-Project/Stream_Spark/spark-streaming-kafka-0-8-assembly_2.11-2.1.0.jar /home/ubuntu/Insight-Project/Stream_Spark/Trades.py '"$ID"'' C-m
 
 
 
